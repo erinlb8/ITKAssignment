b7a17c0fc768dd417d0fce34bb3f0287bfe1cb5c
Erin Bratu <bratuel@gw343.vampire>
2019-10-14 11:07:55 -0500

    Initial code

--- ITKAssignment.cxx
+++ ITKAssignment.cxx
@@ -1,20 +1,29 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 
+// Set up types
+const unsigned int nDims = 3 ;
+typedef itk::Image < int, nDims > ImageType ;
+typedef itk::ImageFileReader < ImageType > ReaderType ;
+
+ImageType::Pointer LoadImage ( const char * inputFile ) {    
+    ReaderType::Pointer reader = ReaderType::New() ;
+    reader->SetFileName( inputFile ) ;
+    reader->Update() ;
+    ImageType::Pointer mvImg = reader->GetOutput() ;
+    return mvImg ;
+}
+
 int main ( int argc, char * argv[] )
 {
     // Verify command line arguments
     if (argc < 3 ) {
 	std::cerr << "useage: " << std::endl ;
-	std::cerr << argv[0] << "inputImageFile outputImageFile" << std::endl ;
+	std::cerr << argv[0] << "inputImages outputImageFile" << std::endl ;
 	return -1 ;
     }
 
-    // Set up types
-    const unsigned int nDims = 3 ;
-    typedef itk::Image < int, nDims > ImageType ;
-
-
+    
 
     return 0 ;
 }
