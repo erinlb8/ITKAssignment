5fc70116424f907c99d02c821613863bbafd6c24
Erin Bratu <bratuel@gw345.vampire>
2019-10-16 14:59:45 -0500

    Fixed averaging

---
+++ AffineRegistration.cxx
@@ -0,0 +1,163 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkResampleImageFilter.h"
+#include "itkAffineTransform.h"
+#include "itkMultiResolutionImageRegistrationMethod.h"
+#include "itkMeanSquaresImageToImageMetric.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkRegularStepGradientDescentOptimizer.h"
+#include <string>
+
+const unsigned int nDims = 3 ;
+typedef itk::Image< float, nDims > ImageType;
+
+class OptimizationObserver : public itk::Command {
+
+public:
+  typedef OptimizationObserver Self ;
+  typedef itk::Command Superclass ;
+  typedef itk::SmartPointer < Self > Pointer ;
+  itkNewMacro( Self ) ;
+  
+  typedef itk::RegularStepGradientDescentOptimizer OptimizerType ;
+  typedef const OptimizerType * OptimizerPointerType ;
+
+  void Execute(itk::Object *caller, const itk::EventObject &event) {
+    Execute ( (const Object *) caller, event ) ;
+  }
+
+  void Execute(const itk::Object *caller, const itk::EventObject &event) {
+    OptimizerPointerType optimizer = dynamic_cast < OptimizerPointerType > ( caller ) ;
+    // std::cout << optimizer->GetCurrentIteration() << " " << optimizer->GetValue() << std::endl ;
+  }
+
+protected:
+  OptimizationObserver() {};
+  
+};
+
+class RegistrationObserver : public itk::Command {
+
+public:
+  typedef RegistrationObserver Self ;
+  typedef itk::Command Superclass ;
+  typedef itk::SmartPointer < Self > Pointer ;
+  itkNewMacro( Self ) ;
+  
+  typedef itk::RegularStepGradientDescentOptimizer OptimizerType ;
+  typedef OptimizerType * OptimizerPointerType ;
+  typedef itk::MultiResolutionImageRegistrationMethod < ImageType, ImageType > RegWrapperType ;
+  typedef RegWrapperType * RegPointerType ;
+
+  void Execute(itk::Object *caller, const itk::EventObject &event) {
+    RegPointerType regWrapper = dynamic_cast < RegPointerType > ( caller ) ;
+    OptimizerPointerType rsgd = dynamic_cast < OptimizerPointerType > ( regWrapper->GetModifiableOptimizer() ) ;
+    
+    //std::cout << "RegObserver" << rsgd->GetCurrentIteration() << std::endl ;
+    
+    int level = regWrapper->GetCurrentLevel() ;
+    if ( level == 0 )
+       rsgd->SetMaximumStepLength ( 0.0625 ) ;
+    else if (level == 1 )
+       rsgd->SetMaximumStepLength ( 0.125 / 2 ) ;
+    else
+       rsgd->SetMaximumStepLength ( 0.25 / 2 ) ;
+  }
+
+  void Execute(const itk::Object *caller, const itk::EventObject &event) {
+ 
+  }
+
+protected:
+  RegistrationObserver() {};
+  
+};
+
+ImageType::Pointer affineRegistration(std::string inFixed, std::string inMoving, std::string outImage )
+{
+  // Verify command line arguments
+
+  // Setup types
+  // Create and setup a reader for moving image
+  typedef itk::ImageFileReader< ImageType >  readerType;
+  readerType::Pointer reader = readerType::New(); 
+  reader->SetFileName( inMoving );
+  reader->Update();
+  ImageType::Pointer movingImage = reader->GetOutput() ;
+
+  // Same for the fixed image
+  readerType::Pointer reader2 = readerType::New() ;
+  reader2->SetFileName ( inFixed ) ;
+  reader2->Update() ;
+  ImageType::Pointer fixedImage = reader2->GetOutput() ;
+
+  // Register images
+  // Set up typedefs
+  typedef itk::AffineTransform < double, 3 > AffineType ;
+  typedef itk::MultiResolutionImageRegistrationMethod < ImageType, ImageType > RegWrapperType ;
+  typedef itk::MeanSquaresImageToImageMetric < ImageType, ImageType > MeanSquares ;
+  typedef itk::LinearInterpolateImageFunction < ImageType, double > LinInterpolate ;
+  typedef itk::RegularStepGradientDescentOptimizer RSGDOpt ;
+
+  // Declare the variables
+  RegWrapperType::Pointer regWrapper = RegWrapperType::New() ;
+  AffineType::Pointer affine = AffineType::New() ;
+  MeanSquares::Pointer msquares = MeanSquares::New() ;
+  LinInterpolate::Pointer linear = LinInterpolate::New() ;
+  RSGDOpt::Pointer rsgd = RSGDOpt::New() ;
+
+  // Optimization Observer
+  OptimizationObserver::Pointer observer = OptimizationObserver::New() ;
+  rsgd->AddObserver ( itk::IterationEvent(), observer );
+
+  // Registration Observer
+  RegistrationObserver::Pointer rObserver = RegistrationObserver::New() ;
+  regWrapper->AddObserver( itk::IterationEvent(), rObserver ) ;
+  // Connect the pipeline
+  affine->SetIdentity() ;
+ 
+  regWrapper->SetMovingImage( movingImage ) ;
+  regWrapper->SetFixedImage( fixedImage ) ;
+  regWrapper->SetTransform( affine ) ;
+  regWrapper->SetMetric( msquares ) ;
+  regWrapper->SetInterpolator( linear ) ;
+  regWrapper->SetOptimizer( rsgd ) ;
+  regWrapper->SetInitialTransformParameters( affine->GetParameters() ) ;
+  regWrapper->SetFixedImageRegion( fixedImage->GetLargestPossibleRegion() );
+  regWrapper->SetNumberOfLevels ( 3 ) ; 
+  // Run the registration
+  try 
+    {  
+      regWrapper->Update() ;
+    }
+  catch( itk::ExceptionObject & excp ) 
+    {
+      std::cerr << "Error in registration" << std::endl;  
+      std::cerr << excp << std::endl; 
+    }
+  
+  // Update the transform
+  affine->SetParameters( regWrapper->GetLastTransformParameters() ) ;
+
+  // Apply the transform
+  typedef itk::ResampleImageFilter < ImageType, ImageType > ResampleFilterType ;
+  ResampleFilterType::Pointer filter = ResampleFilterType::New() ;
+  filter->SetInput ( movingImage ) ;
+
+  filter->SetTransform ( affine ) ;
+  filter->SetSize ( movingImage->GetLargestPossibleRegion().GetSize() ) ;
+  filter->SetReferenceImage ( fixedImage ) ;
+  filter->UseReferenceImageOn() ;
+  filter->Update() ;
+
+  // write out the result to argv[3]
+  typedef itk::ImageFileWriter < ImageType > writerType ;
+  writerType::Pointer writer = writerType::New() ;
+  writer->SetInput ( filter->GetOutput() ) ; 
+  writer->SetFileName ( outImage ) ;
+  writer->Update() ;
+
+  // Done.
+  return filter->GetOutput() ;
+}
--- ITKAssignment.cxx
+++ ITKAssignment.cxx
@@ -1,29 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 #include "itkImage.h"
 #include "itkImageFileReader.h"
+#include "itkAddImageFilter.h"
+#include "itkMultiplyImageFilter.h"
+#include "itkImageFileWriter.h"
+#include "itkDivideImageFilter.h" 
+
 
 // Set up types
 const unsigned int nDims = 3 ;
-typedef itk::Image < int, nDims > ImageType ;
+typedef itk::Image < double, nDims > ImageType ;
 typedef itk::ImageFileReader < ImageType > ReaderType ;
+typedef itk::MultiplyImageFilter < ImageType, ImageType, ImageType > MultiplyType ;
+typedef itk::AddImageFilter < ImageType, ImageType, ImageType > AddType ;
+ 
+ 
+ImageType::Pointer affineRegistration( std::string, std::string, std::string );
 
-ImageType::Pointer LoadImage ( const char * inputFile ) {    
+ImageType::Pointer LoadImage ( std::string inputFile ) {    
     ReaderType::Pointer reader = ReaderType::New() ;
     reader->SetFileName( inputFile ) ;
     reader->Update() ;
-    ImageType::Pointer mvImg = reader->GetOutput() ;
-    return mvImg ;
+    return reader->GetOutput() ;
 }
+
+ImageType::Pointer averageImage( std::string files[] ) {
+    AddType::Pointer adder = AddType::New() ;
+
+    ImageType::Pointer imgTotal = LoadImage( files[0] ) ;
+        
+    for ( int i = 1 ; i < 21 ; i++ ) {
+	adder->SetInput1( imgTotal );
+	adder->SetInput2( LoadImage( files[i] ) );
+	adder->Update() ;
+	imgTotal = adder->GetOutput() ;
+    }
+
+    MultiplyType::Pointer multi = MultiplyType::New() ;
+    multi->SetInput( imgTotal ) ;
+    multi->SetConstant( (double) 1/21 ) ;
+    multi->Update() ;
+    return multi->GetOutput() ; 
+} 
+
 
 int main ( int argc, char * argv[] )
 {
     // Verify command line arguments
     if (argc < 3 ) {
-	std::cerr << "useage: " << std::endl ;
-	std::cerr << argv[0] << "inputImages outputImageFile" << std::endl ;
+	std::cerr << "Usage: " << std::endl ;
+	std::cerr << argv[0] << "inputImageFile outputImageFile" << std::endl ;
 	return -1 ;
     }
 
+    std::fstream f ;
+    f.open( argv[1] ) ;
+    std::string line ;
+    std::string files[21];
+    int c = 0 ;
+    std::string ln2  = "" ;  
+
+    while( getline(f, line) ) {
+	ln2 = "./itk-images/" + line ;
+	files[c] = ln2 ;
+	c++ ;
+    }
     
+
+    //AddType::Pointer adder2 = AddType::New() ;
+ 
+    //ImageType::Pointer randSub = LoadImage( files[0] ) ;
+    //imgTotal = randSub ;
+
+    //for (int i = 0; i < 21; i++ ) {
+//	if (i != r ) {
+    //	    std::string outFile = "./registrations/AR-" + std::string( argv[1] ).substr( 8, 2 ) + ".nii.gz" ;
+	    //t[i] = std::thread(affReg, randSub, files[i], outFile ) ;
+//	    ImageType::Pointer afr = affineRegistration( fixedImage, "./itk-images/" + argv[1], outFile ) ;
+	    
+// 	}
+//    }
+/*
+    for (int i=0; i<21; i++ ) {
+	t[i].join()
+    }
+
+*/
+    ImageType::Pointer finImage = averageImage( files ) ;
+
+//    std::cout << multi->GetConstant() << std::endl ;
+
+    typedef itk::ImageFileWriter < ImageType > WriterType ;
+    WriterType::Pointer writer = WriterType::New() ;
+    writer->SetFileName( argv[2] ) ;
+    writer->SetInput( finImage ) ;
+    writer->Update() ;
 
     return 0 ;
 }
+
+
