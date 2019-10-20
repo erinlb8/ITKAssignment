68ab80d953167f1450650ca75c978f07e84d08ec
Erin Bratu <erinlbratu@gmail.com>
2019-10-20 15:46:16 -0500

    Switched back to Demons

--- DeformableRegistration.cxx
+++ DeformableRegistration.cxx
@@ -1,23 +1,54 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkBSplineTransform.h"
 #include "itkResampleImageFilter.h"
-#include "itkImageRegistrationMethod.h"
-#include "itkMeanSquaresImageToImageMetric.h"
-#include "itkRegularStepGradientDescentOptimizer.h"
-#include "itkLinearInterpolateImageFunction.h"
+#include "itkMultiResolutionPDEDeformableRegistration.h"
+#include "itkMultiResolutionImageRegistrationMethod.h"
+#include "itkDisplacementFieldTransform.h"
+#include "itkHistogramMatchingImageFilter.h"
 #include "itkResampleImageFilter.h"
+#include "itkDemonsRegistrationFilter.h"
 
-#include <pthread.h>
 #include <string>
 
 // Set up types
 const unsigned int nDims = 3 ;
 typedef itk::Image< double, nDims > ImageType ;
 
+class RegistrationObserver : public itk::Command {
+public:
+  typedef RegistrationObserver Self ;
+  typedef itk::Command Superclass ;
+  typedef itk::SmartPointer < Self > Pointer ;
+  itkNewMacro( Self ) ;
+
+  typedef itk::Vector < float, 2 > Vector ;
+  typedef itk::Image < Vector, 2 > DisFieldType ;
+  typedef itk::DemonsRegistrationFilter < ImageType, ImageType, DisFieldType > DemonsType ;
+  typedef DemonsType * DemonsPointer ;
+
+ 
+  void Execute( itk::Object * caller, const itk::EventObject & event ) {
+    DemonsPointer demons = dynamic_cast < DemonsPointer > ( caller ) ;
+    if (!(itk::IterationEvent().CheckEvent( & event ) ) ) {
+      return ;
+    }
+    //std::cout << demons->GetMetric() << std::endl ;
+  }
+
+  void Execute( const itk::Object *caller, const itk::EventObject &event) {
+
+  }
+
+protected:
+  RegistrationObserver() {};
+
+} ;
+
+
 void deformableRegistration( std::string inFixed, std::string inMoving, std::string outImage )
 {
+  std::cout << "Fixed" << std::endl;
 
   typedef itk::ImageFileReader < ImageType > ReaderType ;
   ReaderType::Pointer reader1 = ReaderType::New() ;
@@ -30,60 +61,46 @@ void deformableRegistration( std::string inFixed, std::string inMoving, std::str
   reader2->Update() ;
   ImageType::Pointer moving = reader2->GetOutput() ;
 
-  typedef itk::BSplineTransform < double, nDims, 3 > BSplineType ;
-  typedef itk::MeanSquaresImageToImageMetric < ImageType, ImageType > MetricType ;
-  typedef itk::RegularStepGradientDescentOptimizer OptimizerType ;
-  typedef itk::ImageRegistrationMethod < ImageType, ImageType > RegistrationType ;
-  typedef itk::LinearInterpolateImageFunction < ImageType, double > InterpolatorType ;
+  typedef itk::HistogramMatchingImageFilter < ImageType, ImageType > MatchType ;
+  typedef itk::Vector < double, nDims > Vector ;
+  typedef itk::Image < Vector, nDims > DisType ;
+  typedef itk::DemonsRegistrationFilter < ImageType, ImageType, DisType > Demons ;
+
+  MatchType::Pointer match = MatchType::New() ;
+  Demons::Pointer demons = Demons::New() ;
+  RegistrationObserver::Pointer observer = RegistrationObserver::New() ;
+
+  match->SetInput( moving ) ;
+  match->SetReferenceImage( fixed ) ;
+  match->SetNumberOfHistogramLevels( 1024 ) ;
+  match->SetNumberOfMatchPoints( 7 ) ;
+  match->ThresholdAtMeanIntensityOn() ;
+
+  std::cout << "Moving" << std::endl ;
+
+
+  //demons->AddObserver( itk::IterationEvent(), observer ) ;
+  demons->SetFixedImage( match->GetOutput() ) ;
+  demons->SetMovingImage( moving ) ;
+  demons->SetNumberOfIterations( 50 ) ;
+  demons->SetStandardDeviations( 1.0 ) ;
+
+  std::cout << "Moving" << std::endl ;
+
   
-  BSplineType::Pointer transform = BSplineType::New() ;
-  RegistrationType::Pointer registration = RegistrationType::New() ;
-  MetricType::Pointer meanSquares = MetricType::New() ;
-  OptimizerType::Pointer rsgd = OptimizerType::New() ;
-  InterpolatorType::Pointer linear = InterpolatorType::New() ;
-
-  transform->SetIdentity() ;
-  
-  BSplineType::PhysicalDimensionsType fixedPD ;  
-  BSplineType::MeshSizeType meshSize ;
-  for ( int i = 0 ; i < nDims ; i++ ) { 
-    fixedPD[i] = fixed->GetSpacing()[i] * static_cast<double>(fixed->GetLargestPossibleRegion().GetSize()[i] - 1) ;
-  }
-
-  meshSize.Fill( 5 ) ;
-  
-  registration->SetMovingImage( moving ) ;
-  registration->SetFixedImage( fixed ) ;
-  registration->SetMetric( meanSquares ) ;
-  registration->SetOptimizer( rsgd ) ;
-  registration->SetInterpolator( linear ) ;
-  registration->SetTransform( transform ) ;
-
-  transform->SetTransformDomainOrigin( fixed->GetOrigin() ) ;
-  transform->SetTransformDomainPhysicalDimensions( fixedPD ) ;
-  transform->SetTransformDomainDirection( fixed->GetDirection() ) ;
-  transform->SetTransformDomainMeshSize( meshSize ) ;
-
-  BSplineType::ParametersType parameters( transform->GetNumberOfParameters() ) ;
-  
-  parameters.Fill( 0.0 ) ;
-  transform->SetParameters( parameters ) ;
-  registration->SetInitialTransformParameters( transform->GetParameters() ) ;
-  registration->SetFixedImageRegion( fixed->GetLargestPossibleRegion() ) ;
-
-  rsgd->SetMaximumStepLength( 0.5 ) ;
-  rsgd->SetMinimumStepLength( 0.125 ) ;
-  rsgd->SetNumberOfIterations( 1 ) ;
-
   try { 
-    // std::cout << transform->GetNumberOfParameters() << std::endl ;
-    registration->Update() ;
+// std::cout << transform->GetNumberOfParameters() << std::endl ;
+    demons->Update() ;
   } catch (itk::ExceptionObject & e ) {
     std::cerr << e << std::endl ;
   }
 
-  transform->SetParameters( registration->GetLastTransformParameters() ) ;
   typedef itk::ResampleImageFilter < ImageType, ImageType > ResampleType ;
+  typedef itk::DisplacementFieldTransform < double, nDims > DisplacementType ;
+  
+  DisplacementType::Pointer transform = DisplacementType::New() ;
+  transform->SetDisplacementField( demons->GetOutput() ) ;
+ 
   ResampleType::Pointer resample = ResampleType::New() ;
   resample->SetInput( moving ) ;
   resample->SetTransform( transform ) ;
--- slurm_control.sh
+++ slurm_control.sh
@@ -2,9 +2,9 @@
 id1=$(sbatch --parsable deform_job.slurm)
 sleep 1
 echo "${id1}"
-id2=$(sbatch --parsable --dependency=afterok:"${id1}" avg_deform.slurm "21")
+id2=$(sbatch --parsable --dependency=afterok:"${id1}" avg_deform.slurm "1")
 sleep 1
-for x in {22..40} 
+for x in {2..10} 
 do
     id1=$(sbatch --parsable --dependency=afterok:"${id2}" deform_job.slurm)
     sleep 1
