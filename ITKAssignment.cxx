6018a076b7436c15fc98a7c90e766b872b0531af
Erin Bratu <erinlbratu@gmail.com>
2019-10-20 20:19:32 -0500

    Clean up

--- ITKAssignment.cxx
+++ ITKAssignment.cxx
@@ -7,11 +7,8 @@
 #include "itkAddImageFilter.h"
 #include "itkMultiplyImageFilter.h"
 #include "itkImageFileWriter.h"
-#include "itkDivideImageFilter.h" 
-
 
 // Set up types
-// If using threads, change to be void * 
 void affineRegistration( int, std::string, std::string, std::string ) ;
 void deformableRegistration( std::string, std::string, std::string ) ;
 
@@ -21,32 +18,39 @@ typedef itk::ImageFileReader < ImageType > ReaderType ;
 typedef itk::MultiplyImageFilter < ImageType, ImageType, ImageType > MultiplyType ;
 typedef itk::AddImageFilter < ImageType, ImageType, ImageType > AddType ;
 typedef itk::ImageFileWriter < ImageType > WriterType ;  
+
+ReaderType::Pointer reader = ReaderType::New() ;
+
 using namespace std ;
 
+// Loading Images
 ImageType::Pointer LoadImage ( string inputFile ) {    
-    ReaderType::Pointer reader = ReaderType::New() ;
     reader->SetFileName( inputFile ) ;
     reader->Update() ;
     return reader->GetOutput() ;
 }
 
+// Average a set of images
 int averageImage( vector < string > files, string outFile ) {
     AddType::Pointer adder = AddType::New() ;
     ImageType::Pointer imgTotal = LoadImage( files[0] ) ;
         
+    // Sum of images
     for ( int i = 1 ; i < files.size() ; i++ ) {
 	adder->SetInput1( imgTotal );
 	adder->SetInput2( LoadImage( files[i] ) );
 	adder->Update() ;
 	imgTotal = adder->GetOutput() ;
-	// cout << files[i] << endl ;
     }
 
+    // Averaging
     MultiplyType::Pointer multi = MultiplyType::New() ;
+    double factor = 1/( (double) files.size() ) ;
     multi->SetInput( imgTotal ) ;
-    multi->SetConstant( (double) 1/21 ) ;
+    multi->SetConstant( factor ) ;
     multi->Update() ;
 
+    // Write images
     WriterType::Pointer writer = WriterType::New() ;
     writer->SetFileName( outFile ) ;
     writer->SetInput( multi->GetOutput() ) ;
@@ -59,19 +63,16 @@ int averageImage( vector < string > files, string outFile ) {
 int main ( int argc, char * argv[] )
 {
     // Verify command line arguments
-    if (argc < 1 ) {
+    if (argc < 3 ) {
 	cerr << "Usage: " << endl ;
-	cerr << argv[0] << "inputImagesList outputImageFile methodChoice" << endl ;
+	cerr << argv[0] << "methodSelection deformableRegistrationIndex" << endl ;
 	return -1 ;
     }
 
     int method = atoi(argv[1]) ;
     int ind = atoi(argv[2]) ;
 
-    time_t current_time ;
-    current_time = time( NULL ) ;
-    cout << current_time << endl ;
-
+    // Read base files
     vector < string > files ;
     DIR* dirp = opendir( "./itk-images/" ) ;
     struct dirent * dp ;
@@ -82,57 +83,36 @@ int main ( int argc, char * argv[] )
     }
     closedir( dirp ) ;
 
+    // Attach directory
     string dir1 = "./itk-images/" ;
     for ( int j = 0 ; j < files.size() ; j++ ) {
 	files[j] = dir1 + files[j] ;	
     }
 
+    // Attach directory for registrations
     string dir2 = "./registrations/AR-" ;
-    string dir3 = "./registrations/DR-" ;
     string ft = ".nii.gz" ;
     vector < string > regArray ;
-    vector < string > defArray ;
-//    regArray.push_back( files[0] ) ;
-    
-//    defArray.push_back ( dir3 + files[0].substr( 21, 2 ) + ft ) ;
     
     for ( int i = 0 ; i < files.size() ; i++ ) {
  	regArray.push_back ( dir2 + files[i].substr( 21, 2 ) + ft ) ;
-//        defArray.push_back ( dir3 + files[i].substr( 21, 2 ) + ft ) ;
     }
 
-    if ( strcmp( argv[1], "1" ) == 0 ) {
-	cout << "Method 1" << endl ;
+    if ( method == 1 ) { 		// Calculation of average of original images
 	averageImage( files, "initialAverage.nii.gz" ) ;
     } 
-    else if ( strcmp( argv[1], "2" ) == 0 ) {
-        ImageType::Pointer fixedImage = ImageType::New() ;
-	ReaderType::Pointer reader = ReaderType::New() ;
-	reader->SetFileName( files[0] ) ;
-	reader->Update() ;
-        fixedImage = reader->GetOutput() ;
-
-	cout << "Method 2" << endl ;
+    else if ( method == 2 ) {		// Affine registration and averaging
 	for (int i = 0; i < files.size() ; i++ ) {
 	    affineRegistration( i, files[0], files[i], regArray[i] ) ;
-	    // cout << "Thread " << i << " started" << endl ;
 	}
-
 	averageImage( regArray, "affineAverage.nii.gz" ) ;	
     }
-    else if ( method == 3 ) {
+    else if ( method == 3 ) {		// Demons registration (uses slurm job array)
 	deformableRegistration( "affineAverage.nii.gz", regArray[ind], regArray[ind] ) ;
-	current_time = time( NULL );
-	cout << "Transform " << ind << " Completed " << current_time << endl ;
     }
-    else if ( strcmp( argv[1], "4" ) == 0 ) {
+    else if ( method == 4 ) {		// Deformable averaging
 	averageImage( regArray, "./atlas-images/deformableAverage" + string( argv[2] ) + ".nii.gz" ) ;
     }
 
-    current_time = time( NULL ) ;
-    cout << current_time << endl ;
-
     return 0 ;
 }
-
-
