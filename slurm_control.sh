d186843bc91ec5f92e35aa28a926cb7272b2c1d9
Erin Bratu <erinlbratu@gmail.com>
2019-10-20 20:25:58 -0500

    Modifying slurm/shell scripts

--- slurm_control.sh
+++ slurm_control.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Uses a loop to create dependent jobs for each iteration of the deformable registration
+# avg_deform.slurm jobs receive an input used to identify which iteration's average it
+# belongs to.
 id1=$(sbatch --parsable deform_job.slurm)
 sleep 1
 echo "${id1}"
-id2=$(sbatch --parsable --dependency=afterok:"${id1}" avg_deform.slurm "1")
+id2=$(sbatch --parsable --dependency=afterok:"${id1}" avg_deform.slurm "0")
 sleep 1
-for x in {2..10} 
+for x in {1..99} 
 do
     id1=$(sbatch --parsable --dependency=afterok:"${id2}" deform_job.slurm)
     sleep 1
