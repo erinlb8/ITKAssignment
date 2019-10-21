7ead90b8ad8350d1365bd12bb1af4d1d42893038
Erin Bratu <erin.l.bratu@vanderbilt.edu>
2019-10-20 19:38:19 -0500

    Fixed bug on deformable registration

--- DeformableRegistration.cxx
+++ DeformableRegistration.cxx
@@ -2,8 +2,7 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkResampleImageFilter.h"
-#include "itkMultiResolutionPDEDeformableRegistration.h"
-#include "itkMultiResolutionImageRegistrationMethod.h"
+#include "itkLinearInterpolateImageFunction.h"
 #include "itkDisplacementFieldTransform.h"
 #include "itkHistogramMatchingImageFilter.h"
 #include "itkResampleImageFilter.h"
@@ -15,41 +14,8 @@
 const unsigned int nDims = 3 ;
 typedef itk::Image< double, nDims > ImageType ;
 
-class RegistrationObserver : public itk::Command {
-public:
-  typedef RegistrationObserver Self ;
-  typedef itk::Command Superclass ;
-  typedef itk::SmartPointer < Self > Pointer ;
-  itkNewMacro( Self ) ;
-
-  typedef itk::Vector < float, 2 > Vector ;
-  typedef itk::Image < Vector, 2 > DisFieldType ;
-  typedef itk::DemonsRegistrationFilter < ImageType, ImageType, DisFieldType > DemonsType ;
-  typedef DemonsType * DemonsPointer ;
-
- 
-  void Execute( itk::Object * caller, const itk::EventObject & event ) {
-    DemonsPointer demons = dynamic_cast < DemonsPointer > ( caller ) ;
-    if (!(itk::IterationEvent().CheckEvent( & event ) ) ) {
-      return ;
-    }
-    //std::cout << demons->GetMetric() << std::endl ;
-  }
-
-  void Execute( const itk::Object *caller, const itk::EventObject &event) {
-
-  }
-
-protected:
-  RegistrationObserver() {};
-
-} ;
-
-
 void deformableRegistration( std::string inFixed, std::string inMoving, std::string outImage )
 {
-  std::cout << "Fixed" << std::endl;
-
   typedef itk::ImageFileReader < ImageType > ReaderType ;
   ReaderType::Pointer reader1 = ReaderType::New() ;
   reader1->SetFileName( inFixed ) ;
@@ -65,10 +31,8 @@ void deformableRegistration( std::string inFixed, std::string inMoving, std::str
   typedef itk::Vector < double, nDims > Vector ;
   typedef itk::Image < Vector, nDims > DisType ;
   typedef itk::DemonsRegistrationFilter < ImageType, ImageType, DisType > Demons ;
-
   MatchType::Pointer match = MatchType::New() ;
   Demons::Pointer demons = Demons::New() ;
-  RegistrationObserver::Pointer observer = RegistrationObserver::New() ;
 
   match->SetInput( moving ) ;
   match->SetReferenceImage( fixed ) ;
@@ -76,20 +40,12 @@ void deformableRegistration( std::string inFixed, std::string inMoving, std::str
   match->SetNumberOfMatchPoints( 7 ) ;
   match->ThresholdAtMeanIntensityOn() ;
 
-  std::cout << "Moving" << std::endl ;
-
-
-  //demons->AddObserver( itk::IterationEvent(), observer ) ;
-  demons->SetFixedImage( match->GetOutput() ) ;
-  demons->SetMovingImage( moving ) ;
+  demons->SetFixedImage( fixed ) ;
+  demons->SetMovingImage( match->GetOutput() ) ;
   demons->SetNumberOfIterations( 50 ) ;
   demons->SetStandardDeviations( 1.0 ) ;
 
-  std::cout << "Moving" << std::endl ;
-
-  
   try { 
-// std::cout << transform->GetNumberOfParameters() << std::endl ;
     demons->Update() ;
   } catch (itk::ExceptionObject & e ) {
     std::cerr << e << std::endl ;
@@ -97,18 +53,21 @@ void deformableRegistration( std::string inFixed, std::string inMoving, std::str
 
   typedef itk::ResampleImageFilter < ImageType, ImageType > ResampleType ;
   typedef itk::DisplacementFieldTransform < double, nDims > DisplacementType ;
-  
+  typedef itk::LinearInterpolateImageFunction < ImageType, double > LinearType ;
+
+  LinearType::Pointer linear = LinearType::New() ;
   DisplacementType::Pointer transform = DisplacementType::New() ;
-  transform->SetDisplacementField( demons->GetOutput() ) ;
- 
   ResampleType::Pointer resample = ResampleType::New() ;
+
+  transform->SetDisplacementField( demons->GetOutput() ) ;
+  
   resample->SetInput( moving ) ;
   resample->SetTransform( transform ) ;
+  resample->SetInterpolator( linear ) ;
   resample->SetSize( moving->GetLargestPossibleRegion().GetSize() ) ;
   resample->SetReferenceImage( fixed ) ;
   resample->UseReferenceImageOn() ;
   resample->Update() ;
-
 
   typedef itk::ImageFileWriter < ImageType > WriterType ;
   WriterType::Pointer writer = WriterType::New() ;
